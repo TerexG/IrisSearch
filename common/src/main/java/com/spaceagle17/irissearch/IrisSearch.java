ff9ead490a5bf90e9a04117ebd04eefa9ddc16cc
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2026-06-23 08:54:34 +0200

    1.1.0

--- common/src/main/java/com/spaceagle17/irissearch/IrisSearch.java
+++ common/src/main/java/com/spaceagle17/irissearch/IrisSearch.java
@@ -5,7 +5,7 @@ import com.spaceagle17.irissearch.logging.IrisSearchLogger;
 import java.nio.file.Path;
 
 public class IrisSearch {
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
 
     // Get necessary paths
     public static Path configDirectory = ModLoaderSpecifics.configDirectory();
